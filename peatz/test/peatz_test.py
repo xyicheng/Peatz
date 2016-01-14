d2e17152d382118e0fa76f1eefd1a9484fc7836e
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2016-01-14 01:26:51 +0100

    created entry file

---
+++ peatz/test/peatz_test.py
@@ -0,0 +1,24 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+import unittest
+
+
+class  Peatz_TestCase(unittest.TestCase):
+    #def setUp(self):
+    #    self.foo = Peatz_()
+    #
+
+    #def tearDown(self):
+    #    self.foo.dispose()
+    #    self.foo = None
+
+    def test_peatz_(self):
+        #assert x != y;
+        #self.assertEqual(x, y, "Msg");
+        self.fail("TODO: Write test")
+
+if __name__ == '__main__':
+    unittest.main()
+
